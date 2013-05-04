96e3537c935dbd37756eb939b2c80364e9ae2ea9
Srichand Pendyala <srichand.pendyala@gmail.com>
2013-05-04 00:41:27 -0700

    Initial controller code.
    The controller supplies decisions to the trading agent based on ticks
    from a price source. The decision making kernel needs to be abstracted
    out of this. Also, the knobs (lower and upper bounds) are currently hard
    coded and need to be abstracted too.

--- src/interface.py
+++ src/interface.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python
 
-class interface:
-  def __init__(self):
-    self.buy=self.get_lower()
-    self.sell=self.get_upper()
-  def get_lower(self):
-    f=open('lower_value.txt','r')
-    val=f.read()
-    f.close()
-    return int(val)
-  def get_upper(self):
-    f=open('upper_value.txt','r')
-    val=f.read()
-    f.close()
-    return int(val)
-  def what_to_do(self, value):
-    if value >= self.sell:
-      print("SELL!")
-      return
-    if value <= self.buy:
-      print("BUY!!")
-      return
-    print("Dont do anything right now")
+from price_source import RandomPriceSource
+from agent import Agent
 
+class Controller:
+  '''
+    The Controller is the entry point to the system
+    and acts as the market.
+  '''
+  def __init__(self, agent, lower, upper):
+    self.agent = agent
+    self.lower = lower
+    self.upper = upper
+
+  def what_to_do(self, value):
+    lower_bound, upper_bound = self.lower(), self.upper()
+    if value < lower_bound:
+      self.agent.buy(value, lower_bound)
+    elif value >= upper_bound:
+      self.agent.sell(value, upper_bound)
+    else:
+      self.agent.hold(value, lower_bound, upper_bound)
+
+def main():
+  o, h, l, c = 100, 130, 70, 90
+  source = RandomPriceSource(o, h, l, c)
+
+  lower_bound = lambda: o - 10
+  upper_bound = lambda: o + 10
+
+  agent = Agent()
+  controller = Controller(agent, lower_bound, upper_bound)
+
+  for price in source:
+    controller.what_to_do(*price)
 
 if __name__== "__main__":
-  trader=interface()
-  price=raw_input("whats the current price?")
-  trader.what_to_do(int(price))
-
+  main()
 
 
